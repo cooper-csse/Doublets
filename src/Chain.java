b08e62cfbaaa8d30ddb7d5d6d61f9921d034cebf
Aidan A McKnight <mcknigaa@rose-hulman.edu>
2018-12-15 19:15:45 -0500

    implemented chain

--- src/Chain.java
+++ src/Chain.java
@@ -1,23 +1,69 @@
 import java.util.Iterator;
 
 public class Chain {
-	public Chain addLast(String test) {
-		return null;
+	
+	private int length = 0;
+	private String value;
+	private Chain prev;
+	private Chain next;
+	private Chain head;
+	
+	public Chain(){
+		value = null;
+		prev = null;
+		next = null;
+		head = this;
+	}
+	
+	
+	public Chain addLast(String word) {
+		Chain chain = new Chain();
+		chain.head = this.head;
+		chain.value = word;
+		chain.length = this.length + 1;
+		this.next = chain;
+		chain.prev = this;
+		return chain;
 	}
 
-	public boolean contains(String test) {
+	public boolean contains(String word) {
+		Iterator iterator = this.iterator();
+		while (iterator.hasNext()){
+			if(iterator.next() == word)
+				return true;
+		}
+			
 		return false;
 	}
 
 	public Iterator<String> iterator() {
-		return null;
+		return new ChainIterator(this);
 	}
 
 	public String getLast() {
-		return null;
+		return this.value;
 	}
 
 	public int length() {
-		return 0;
+		return this.length;
+	}
+	
+	class ChainIterator implements Iterator<String> {
+		Chain current;
+		
+		public ChainIterator (Chain chain){
+			this.current = chain.head;
+		}
+		
+		@Override
+		public boolean hasNext () {
+			return this.current.next != null;
+		}
+		
+		@Override
+		public String next() {
+			this.current = this.current.next;
+			return this.current.value;
+		}
 	}
 }
