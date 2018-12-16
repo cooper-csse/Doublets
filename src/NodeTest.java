15020eaf992bb9fff932f82cfb8152110eb488dd
Cooper Anderson <cooper@cooperstuff.ninja>
2018-12-15 20:56:40 -0500

    Optimized Tree Creation

--- src/NodeTest.java
+++ src/NodeTest.java
@@ -1,12 +1,41 @@
-import org.junit.Before;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class NodeTest {
 	@Test
-	public void maintest() {
-		//Links linksTiny = new Links("tiny.dictionary.txt");
-		Links linksTiny = new Links("english.cleaned.all.10.txt");
-		//System.out.println(linksTiny.getCandidates("are"));
-		System.out.println(linksTiny);
+	public void testTinyDictionaryRuntime() {
+		double elapsedTime;
+		double startTime = System.currentTimeMillis();
+		Links linksTiny = new Links("tiny.dictionary.txt");
+		elapsedTime = (System.currentTimeMillis() - startTime);
+		assertTrue(elapsedTime < 10);
+	}
+
+	@Test
+	public void testEnglishCleanedAll10Runtime() {
+		double elapsedTime;
+		double startTime = System.currentTimeMillis();
+		Links links10 = new Links("english.cleaned.all.10.txt");
+		elapsedTime = (System.currentTimeMillis() - startTime);
+		assertTrue(elapsedTime < 100);
+	}
+
+	@Test
+	public void testEnglishCleanedAll20Runtime() {
+		double elapsedTime;
+		double startTime = System.currentTimeMillis();
+		Links links20 = new Links("english.cleaned.all.20.txt");
+		elapsedTime = (System.currentTimeMillis() - startTime);
+		assertTrue(elapsedTime < 1000);
+	}
+
+	@Test
+	public void testEnglishCleanedAll35Runtime() {
+		double elapsedTime;
+		double startTime = System.currentTimeMillis();
+		Links links35 = new Links("english.cleaned.all.35.txt");
+		elapsedTime = (System.currentTimeMillis() - startTime);
+		assertTrue(elapsedTime < 30000);
 	}
 }
