7e7a9fa28b23f00019addbe0ba14e63119cb4a15
mcknigaa <mcknigaa@rose-hulman.edu>
2018-12-15 20:13:54 -0500

    implented queue chain manager

--- src/QueueChainManager.java
+++ src/QueueChainManager.java
@@ -1,16 +1,28 @@
+import java.util.LinkedList;
+
 public class QueueChainManager extends ChainManager {
+	
+	private LinkedList <Chain> queue;
+	
+	public QueueChainManager (){
+		this.queue = new LinkedList<>();
+	}
+	
 	@Override
 	public void add(Chain chain) {
-
+		this.queue.addLast(chain);
+		this.updateMax(this.queue.size());
 	}
 
 	@Override
 	public Chain next() {
-		return null;
+		if (this.queue.size() == 0)
+			return null;
+		return this.queue.size() == 0 ? null : this.queue.removeFirst();
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return false;
+		return this.queue.size() == 0;
 	}
 }
